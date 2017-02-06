9d2bc981fff561e742c88c0de4db2e47c96cdae9
trangntt1108 <trangntt1108@gmail.com>
2017-02-06 16:56:01 +0700

    commit

---
+++ QuanLyHocTap/QuanLyHocTap/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyHocTap.Startup))]
+namespace QuanLyHocTap
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
